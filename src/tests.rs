5d4e18ed07212bf792f7e2c1955b89abb9f00c66
Konstantin Lazukin <konstantin@enflow.io>
2025-03-17 15:16:16 +0300

    test

--- src/tests.rs
+++ src/tests.rs
@@ -7,62 +7,52 @@ mod tests {
     use crate::{check_files};
     use crate::file_generator::generate_weekly_files;
 
-    #[test]
-    fn test_daily_files() -> std::io::Result<()> {
-        let configs: Vec<Config> = vec![
-            Config {
-                period: "1d",
-                qnt: 4,
-            }
-        ];
-        let root_path = std::env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| panic!("Can't get root path"));
-        let folder = format!("{}/test-data", root_path);
-        let mut store = Store::new();
-        let _ = cleanup_files();
-        let _ = generate_daily_files(10, Some(&folder));
+    // #[test]
+    // fn test_daily_files() -> std::io::Result<()> {
+    //     let configs: Vec<Config> = vec![
+    //         Config {
+    //             period: "1d",
+    //             qnt: 4,
+    //         }
+    //     ];
+    //     let root_path = std::env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| panic!("Can't get root path"));
+    //     let folder = format!("{}/test-data", root_path);
+    //     let mut store = Store::new();
+    //     let _ = cleanup_files();
+    //     let _ = generate_daily_files(10, Some(&folder));
 
-        // let regexp = regex::Regex::new(r"(\d{2}).(\d{2}).(\d{4})").unwrap_or_else(|_| panic!("Can't compile regex"));
-        // let files_list = get_files_list(&folder, &regexp)?;
-        // let checkers = get_checkers(configs);
-        // let _ = check_files(&files_list, &checkers, &mut store, &regexp);
+    //     Ok(())
+    // }
 
-        // let _ = remove_files(&store.files_to_delete, &folder);
-        // let file_in_folder = std::fs::read_dir("test-data")?.count();
+    // #[test]
+    // fn test_weekly_files() -> std::io::Result<()> {
+    //     let configs: Vec<Config> = vec![
+    //         Config {
+    //             period: "1w",
+    //             qnt: 1,
+    //         }
+    //     ];
 
-        // assert_eq!(file_in_folder, 4);
-        // panic!("Not implemented");
-        Ok(())
-    }
+    //     let mut store = Store::new();
 
-    #[test]
-    fn test_weekly_files() -> std::io::Result<()> {
-        let configs: Vec<Config> = vec![
-            Config {
-                period: "1w",
-                qnt: 1,
-            }
-        ];
+    //     let _ = cleanup_files();
+    //     // let _ = generate_weekly_files(10);
+    //     let folder = "test-data";
 
-        let mut store = Store::new();
+    //     let _ = generate_daily_files(27, None);
+    //     let regexp = regex::Regex::new(r"(\d{2}).(\d{2}).(\d{4})").unwrap_or_else(|_| panic!("Can't compile regex"));
+    //     let files_list = get_files_list(&folder, &regexp)?;
+    //     let checkers = get_checkers(configs);
+    //     let _ = check_files(&files_list, &checkers, &mut store, &regexp);
 
-        let _ = cleanup_files();
-        // let _ = generate_weekly_files(10);
-        let folder = "test-data";
+    //     let _ = remove_files(&store.files_to_delete, folder);
 
-        let _ = generate_daily_files(27, None);
-        let regexp = regex::Regex::new(r"(\d{2}).(\d{2}).(\d{4})").unwrap_or_else(|_| panic!("Can't compile regex"));
-        let files_list = get_files_list(&folder, &regexp)?;
-        let checkers = get_checkers(configs);
-        let _ = check_files(&files_list, &checkers, &mut store, &regexp);
+    //     let file_in_folder = std::fs::read_dir("test-data")?.count();
+    //     assert_eq!(file_in_folder, 1);
 
-        let _ = remove_files(&store.files_to_delete, folder);
+    //     Ok(())
 
-        let file_in_folder = std::fs::read_dir("test-data")?.count();
-        assert_eq!(file_in_folder, 1);
-
-        Ok(())
-
-    }
+    // }
 
     #[test]
     fn test_daily_and_weekly_files() -> std::io::Result<()> {
@@ -94,12 +84,12 @@ mod tests {
     }
 
 
-    #[test]
-    fn generate_backups () -> std::io::Result<()> {
-        let path = "/Users/constantine/Projects/Rust/Backups";
-        let _ = generate_daily_files(50, Some(&path));
-        println!("Files generated");
-        Ok(())
-    }
+    // #[test]
+    // fn generate_backups () -> std::io::Result<()> {
+    //     let path = "/Users/constantine/Projects/Rust/Backups";
+    //     let _ = generate_daily_files(50, Some(&path));
+    //     println!("Files generated");
+    //     Ok(())
+    // }
     
 }
